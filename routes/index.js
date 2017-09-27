f5f264ca0a66e579a345b80ac57d4f751d289d65
Rubi Henjaya <rubi.henjaya@gmail.com>
2017-09-27 15:52:53 +0700

    login, register done

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
-var express = require('express');
-var router = express.Router();
+"use strict"
+const express = require('express');
+const router = express.Router();
+const isLoggedIn = require('../helpers/auth')
 
 /* GET home page. */
 module.exports = function(passport){
@@ -7,9 +9,20 @@ module.exports = function(passport){
     res.render('index');
   });
 
+  router.get('/logout', function(req, res){
+    req.logout();
+    res.redirect('/');
+  });
+
   router.get('/login', function(req, res, next) {
     res.render('login', {message: req.flash('loginMessage')});
   });
+
+  router.post('/login', passport.authenticate('local-login', {
+    successRedirect: '/profile',
+    failureRedirect: '/login',
+    failureFlash: true
+  }));
 
   router.get('/signup', function(req, res, next) {
     res.render('signup', {message: req.flash('signupMessage')});
@@ -20,6 +33,10 @@ module.exports = function(passport){
     failureRedirect: '/signup',
     failureFlash: true
   }));
+
+  router.get('/profile', isLoggedIn, function(req, res){
+    res.render('profile', {user: req.user})
+  });
 
   return router;
 }
